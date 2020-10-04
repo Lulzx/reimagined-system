6eb4e435582962c540c0a1ce527fc15d5599e1c1
Lulzx <wiserishabh@gmail.com>
2020-10-05 02:07:34 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,57 +1,37 @@
+from teleflask import Teleflask
+from teleflask.messages import TextMessage
 import os
-import random
-import logging
-import telegram
 
-from flask import Flask, jsonify, Response, request
-app = Flask(__name__)
+API_KEY = os.getenv("TELEGRAM_TOKEN")
+bot = Teleflask(API_KEY, app)
+
+@app.route("/")
+def index():
+    return "This is awesome, isn't it?"
+# end def
 
 
-def generate_cats():    
-    cs_all = [
-        "👊",
-        "💪",
-        "⭐️",
-        "⚡️",
-        "😍",
-        "😱",
-        "🤙",
-        "♻️",
-    ]
-    
-    cs = random.sample(cs_all, 3)
-    
-    t = f"""
-{cs[0]}: {random.randint(4,5)}/5
-{cs[1]}: {random.randint(3,5)}/5
-{cs[2]}: {random.randint(1,5)}/5
-    """
-        
-    return t
-        
-    
-@app.route('/me', methods=['GET'])
-def getme():
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    bot = telegram.Bot(TELEGRAM_TOKEN)
-    return str(bot.get_me())
-    
+# Register the /start command
+@bot.command("start")
+def start(update, text):
+    # update is the update object. It is of type pytgbot.api_types.receivable.updates.Update
+    # text is the text after the command. Can be empty. Type is str.
+    return TextMessage("<b>Hello!</b> Thanks for using @" + bot.username + "!", parse_mode="html")
+# end def
 
-@app.route('/api', methods=['GET', 'POST'])
-def api():
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    if TELEGRAM_TOKEN is None:
-        return jsonify({"status": "error", "reason": "no tg token"})
-        
-    bot = telegram.Bot(TELEGRAM_TOKEN)
-    
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
 
-        text = generate_cats()  # update.message.text 
-        bot.sendMessage(chat_id=chat_id, text=text)
-    else:
-        return str(bot.get_me())
-        
-    return jsonify({"status": "ok"})
+# register a function to be called for updates.
+@bot.on_update
+def foo(update):
+    from pytgbot.api_types.receivable.updates import Update
+    assert isinstance(update, Update)
+    # do stuff with the update
+    # you can use bot.bot to access the pytgbot.Bot's messages functions
+    if not update.message:
+        return
+        # you could use @bot.on_message instead of this if.
+    # end if
+    if update.message.new_chat_member:
+        return TextMessage("Welcome!")
+    # end if
+# end def
