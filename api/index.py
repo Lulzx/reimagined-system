eefef3cad381454913e583e44eb987531a2c6a68
Lulzx <wiserishabh@gmail.com>
2020-10-05 01:50:34 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,57 @@
-from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+import os
+import random
+import logging
+import telegram
 
-class handler(BaseHTTPRequestHandler):
+from flask import Flask, jsonify, Response, request
+app = Flask(__name__)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('Hello darkness my old friend!')
-        self.wfile.write(message.encode())
-        return
+
+def generate_cats():    
+    cs_all = [
+        "👊",
+        "💪",
+        "⭐️",
+        "⚡️",
+        "😍",
+        "😱",
+        "🤙",
+        "♻️",
+    ]
+    
+    cs = random.sample(cs_all, 3)
+    
+    t = f"""
+{cs[0]}: {random.randint(4,5)}/5
+{cs[1]}: {random.randint(3,5)}/5
+{cs[2]}: {random.randint(1,5)}/5
+    """
+        
+    return t
+        
+    
+@app.route('/', methods=['GET'])
+def getme():
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    bot = telegram.Bot(TELEGRAM_TOKEN)
+    return str(bot.get_me())
+    
+
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    if TELEGRAM_TOKEN is None:
+        return jsonify({"status": "error", "reason": "no tg token"})
+        
+    bot = telegram.Bot(TELEGRAM_TOKEN)
+    
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.message.chat.id
+
+        text = generate_cats()  # update.message.text 
+        bot.sendMessage(chat_id=chat_id, text=text)
+    else:
+        return str(bot.get_me())
+        
+    return jsonify({"status": "ok"})
